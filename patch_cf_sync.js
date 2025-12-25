c1d8334ac3368ae550ec341ed86b971f36bf3073
coemidis2 <coemidis@midis.gob.pe>
2025-12-25 12:28:39 -0500

    Versión 3

--- patch_cf_sync.js
+++ patch_cf_sync.js
@@ -1,3 +1,6 @@
 // patch_cf_sync.js
 window.__API_CFG = window.__API_CFG || {};
+// URL base de tu Cloudflare Worker (API)
 window.__API_CFG.baseUrl = "https://coe-midis-api.coemidis2.workers.dev";
+// Timeout de red (ms)
+window.__API_CFG.timeoutMs = 15000;
