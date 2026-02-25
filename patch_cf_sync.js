cc0e2f017f6b506a9eb787c99335c6a31ae7dd0c
coemidis2 <coemidis@midis.gob.pe>
2026-02-24 22:14:18 -0500

    Versión 6

--- patch_cf_sync.js
+++ patch_cf_sync.js
@@ -2,4 +2,4 @@
 // Configuración de API para el Front (GitHub Pages)
 window.__API_CFG = window.__API_CFG || {};
 window.__API_CFG.baseUrl = "https://coe-midis-api.coemidis2.workers.dev";
-window.__API_CFG.timeoutMs = 15000;
+window.__API_CFG.timeoutMs = 120000;
