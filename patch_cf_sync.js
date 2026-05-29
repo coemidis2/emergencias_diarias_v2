ca3cea7339bbc080985a64498cf45fe7c6a68c49
coemidis2 <coemidis@midis.gob.pe>
2026-05-29 16:02:38 -0500

    Versión 222

--- patch_cf_sync.js
+++ patch_cf_sync.js
@@ -1,4 +1,4 @@
-// patch_cf_sync.js - list/detail compatible
+// patch_cf_sync.js
 // Configuración de API para el Front (GitHub Pages)
 window.__API_CFG = window.__API_CFG || {};
 window.__API_CFG.baseUrl = "https://coe-midis-api.coemidis2.workers.dev";
